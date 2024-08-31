2a21ca5a86196b9464bd27c2c50e3048208f4021
Smile ThKing <smilethking@Smiles-MacBook-Air.local>
2024-08-31 17:37:48 -0500

    adject the hero  height

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import {FaGithub } from 'react-icons/fa';
 import { FaFacebookF, FaX,FaLinkedinIn } from 'react-icons/fa6';
-import { myPicture } from '../assets/assetFiles';
+import { me, myPicture } from '../assets/assetFiles';
 import Button from './Button';
 import { IoIosArrowDown } from 'react-icons/io';
 
 const Hero = () => {
   return (
-    <section className=" w-full md:h-dvh h-full p-4 flex flex-col  text-white">
-      <div className="flex flex-col md:flex-row  items-center">
-      <div className="left-side w-full md:w-2/5 h-full flex flex-col justify-center gap-y-3 md:gap-y-8 ">
+    <section className=" w-full md:h-dvh h-full p-4 flex flex-col justify-evenly  text-white">
+      <div className="flex flex-col md:flex-row items-center">
+      <div className="left-side w-full md:w-2/3 h-full flex flex-col justify-center gap-y-8 ">
           <h4 className='uppercase'>
             Hi there! i'm
           </h4>
@@ -22,9 +22,12 @@ const Hero = () => {
 
 
             <div className="flex flex-col md:flex-row  md:items-center justify-between md:w-[400px] gap-4 md:gap-0">
+              <div className=" w-fit">
               <Button title={'Resume'}/>
+              </div>
+             
 
-              <div className="social-media text-xl md:text-3xl flex gap-4 ">
+              <div className="social-media text-xl md:text-3xl flex gap-6 ">
               <FaGithub/>
               <FaLinkedinIn/>
               <FaX/>
@@ -37,13 +40,13 @@ const Hero = () => {
             
         </div>
         <div className="right-side flex  md:grid place-content-center place-items-start md:w-3/5 w-full h-fit  bg-green-600/0">
-            <img className=" w-[180px] md:w-[350px] h-full  object-cover" src={myPicture} alt="my picture" />
+            <img className=" w-[180px] md:w-[400px] h-full  object-cover" src={me} alt="my picture" />
         </div>
 
       </div>
         
 
-        <div className="text-orange-500 text-3xl flex self-center animate-bounce">
+        <div className="text-orange-500 text-3xl flex self-center animate-bounce mb-8">
           <IoIosArrowDown/>
         </div>
     </section>
