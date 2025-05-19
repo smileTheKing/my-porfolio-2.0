b7662a9a177c656817146602db78067594dce0f7
Solomon Cammue <s.cammue@gmail.com>
2025-05-18 19:41:40 -0500

    fixed gap spacing for small devices

--- src/pages/ProjectPage.jsx
+++ src/pages/ProjectPage.jsx
@@ -62,7 +62,7 @@ const ProjectPage = () => {
     }}}
     animate={{
       // opacity:1
-    }}  className="flex flex-col gap-6  w-full h-screen  text-white  bg-red-5f00 pb-20">
+    }}  className="flex flex-col gap-6  w-full min-h-screen  text-white  bg-red-5f00 pb-20">
         <h2 className="text-4xl font-bold text-center h-[80px] bg-slate-900/0 flex justify-center items-center uppercase rounded-md">My Projects</h2>
         <div className='mx-auto font-mono'>
             {/* isMobile */}
